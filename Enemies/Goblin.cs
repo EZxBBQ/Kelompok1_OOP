0b250909e499ffb24fc7583d849e70852401ee96
Adi Nugroho <141153656+EZxBBQ@users.noreply.github.com>
2024-12-07 21:50:03 +0700

    Delete Enemies directory

--- Enemies/Goblin.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Goblin : Enemy
-{
-    public Goblin(int level)
-        : base("Goblin", level, 50 + level * 10, 10 + level * 2, 5 + level, 20 + level * 5, 10 + level * 2, "Goblin Ear")
-    {
-    }
-}
--- Enemies/Orc.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Orc : Enemy
-{
-    public Orc(int level)
-        : base("Orc", level, 100 + level * 20, 20 + level * 3, 10 + level * 2, 50 + level * 10, 20 + level * 5, "Orc Tooth")
-    {
-    }
-}
